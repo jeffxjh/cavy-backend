2b08d23851266498b43bef296cfb7a8edfda7970
jeffxjh <jqueryxjh@gmail.com>
2021-07-04 13:18:39 +0800

    feat: 使用sessionid

--- src/main/java/com/jh/cavybackend/api/BusinessApi.java
+++ src/main/java/com/jh/cavybackend/api/BusinessApi.java
@@ -1,5 +1,6 @@
 package com.jh.cavybackend.api;
 
+import cn.hutool.core.util.StrUtil;
 import com.jh.cavybackend.redis.CacheService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("/business")
@@ -28,7 +32,13 @@ public class BusinessApi {
     }
 
     @GetMapping("/")
-    public String get(@RequestParam String uuid) throws InterruptedException {
+    public Map<String, String> get(@RequestParam String uuid, HttpServletRequest request) throws InterruptedException {
+        Map<String, String> result = new HashMap<>(2);
+        if (StrUtil.isEmpty(uuid)) {
+            uuid = request.getSession().getId();
+        }
+        result.put("uuid", uuid);
+        result.put("count", "100");
         if (cacheService.hasKey(uuid)) {
             log.info("查询执行进度");
             String finish = cacheService.get(uuid).toString();
@@ -36,13 +46,13 @@ public class BusinessApi {
                 //这里给前端一个已完成的信号 然后删除key
                 cacheService.del(uuid);
             }
-            return finish;
+            result.put("count", finish);
         } else {
             log.info("开始执行任务");
             Business business = new Business(uuid);
             new Thread(business).start();
         }
-        return "100";
+        return result;
     }
 
     static class Business implements Runnable {
@@ -63,7 +73,7 @@ public class BusinessApi {
             //模拟业务操作
             while (count <= 100) {
                 log.info("任务进度{}。。", count + "");
-                Thread.sleep(50);
+                Thread.sleep(200);
                 //用过期时间限制重复操作
                 cacheService.set(uuid, count, 60);
                 count++;
