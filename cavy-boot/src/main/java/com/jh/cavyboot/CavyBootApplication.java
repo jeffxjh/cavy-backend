be95ad0f8ec96bd0617c1f17e9436280fc2db1e3
jeffxjh <jqueryxjh@gmail.com>
2021-08-03 21:44:14 +0800

    feat: 继承nacos

--- cavy-boot/src/main/java/com/jh/cavyboot/CavyBootApplication.java
+++ cavy-boot/src/main/java/com/jh/cavyboot/CavyBootApplication.java
@@ -2,12 +2,14 @@ package com.jh.cavyboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 
 @Configuration
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.jh.configure"})
 public class CavyBootApplication {
