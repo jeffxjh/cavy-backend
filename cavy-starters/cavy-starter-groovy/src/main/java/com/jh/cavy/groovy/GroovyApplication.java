eb453c205929c065c69658813546a2c1ff732928
jeffxjh <jqueryxjh@gmail.com>
2025-01-01 19:50:24 +0800

    feat: 动态加载groovy脚本

--- cavy-starters/cavy-starter-groovy/src/main/java/com/jh/cavy/groovy/GroovyApplication.java
+++ cavy-starters/cavy-starter-groovy/src/main/java/com/jh/cavy/groovy/GroovyApplication.java
@@ -1,16 +1,19 @@
 package com.jh.cavy.groovy;
 
 import com.jh.cavy.groovy.service.ScriptInterface;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 @EnableAutoConfiguration
 public class GroovyApplication implements ApplicationRunner {
@@ -18,11 +21,13 @@ public class GroovyApplication implements ApplicationRunner {
         SpringApplication.run(GroovyApplication.class, args);
     }
 
+    //@Qualifier(value = "scriptInterfaceImpl")
     @Autowired
     ScriptInterface scriptInterface;
     @Autowired
     private GroovyLoader groovyLoader;
-
+    @Autowired
+    private  ApplicationContext applicationContext;
     @RequestMapping("/loadBean")
     public String loadBean(@RequestBody LoadBeanInfo loadBeanInfo) {
         String beanName = loadBeanInfo.getBeanName();
@@ -34,6 +39,12 @@ public class GroovyApplication implements ApplicationRunner {
 
     @RequestMapping("/getName")
     public String getName(@RequestBody LoadBeanInfo loadBeanInfo) {
+        String[] names = applicationContext.getBeanDefinitionNames();
+        int index = 1;
+        for (String name : names) {
+        log.debug("{}: {}", index++ , name );
+        }
+        log.debug("项目启动 容器注入javaBean:{}个.", names.length);
         return scriptInterface.getName();
     }
 
